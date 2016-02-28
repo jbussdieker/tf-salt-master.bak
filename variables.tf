97554be350d8f96de46a021ca85c5affabab97f2
Joshua B. Bussdieker <jbussdieker@gmail.com>
2016-02-28 13:23:48 -0800

    Add variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,19 @@ variable "instance_type" {
 variable "key_name" {
   default = ""
 }
+
+variable "env" {}
+variable "github_user" {}
+variable "github_password" {}
+variable "user_name" {}
+variable "user_email" {}
+variable "salt_repo_url" {}
+
+variable "salt_repo_revision" {
+  default = "master"
+}
+
+variable "pillar_repo_url" {}
+variable "pillar_repo_revision" {
+  default = "master"
+}
